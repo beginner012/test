c24d2d949d1443587959d0e3c236f708677f9592
beginner012 <237112207@qq.com>
2019-06-08 15:42:27 +0800

    fix:test文件增加一行输出代码

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         System.out.println("通过rebase -i 回来对第一次的提交进行修改");
+
+        System.out.println("增加一个输出");
+
 
     }
 }
