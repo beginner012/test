16d22771e12724c37fe0e0eb34a6e24e075eb081
beginner012 <237112207@qq.com>
2019-06-08 15:41:27 +0800

    fix:删除test文件多余代码(使用rebase-i修改)

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        System.out.println("通过rebase -i 回来对第一次的提交进行修改");
+
     }
 }
