0aab6233b17a43c3cccd6e9aee78804ef3ac4961
beginner012 <237112207@qq.com>
2019-06-19 23:03:27 +0800

    commit:1

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        System.out.println("通过rebase -i 回来对第一次的提交进行修改");
+
+        System.out.println("增加一个输出");
+
+        int a=100;
+
     }
 }
